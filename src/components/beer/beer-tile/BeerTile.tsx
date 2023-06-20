c63cc1dd913de13d3e8b55f157fd7c323e3113d6
Hubert Cywka <hcywka@gmail.com>
2023-06-20 20:45:29 +0200

    EX-4: Added BeerTile component

--- src/components/beer/beer-tile/BeerTile.tsx
+++ src/components/beer/beer-tile/BeerTile.tsx
@@ -1,12 +1,46 @@
 import { ComponentProps } from 'react';
 import Beer from '../../../helpers/interfaces/Beer.interface';
+import './BeerTile.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import appRouter from '../../../router/AppRouter';
+import { ROUTE } from '../../../constants/RouteConstants';
+import Chip from '../../primitives/chip/Chip';
 
 interface BeerTileProps extends ComponentProps<'div'> {
   beer: Beer;
 }
 
 const BeerTile = ({ beer }: BeerTileProps) => {
-  return <div>{beer.name}</div>;
+  const redirectToDetailsPage = () => {
+    appRouter.navigate(ROUTE.DETAILS.replace(':id', beer.id.toString()));
+  };
+
+  return (
+    <div className="beer-tile-container">
+      <FontAwesomeIcon
+        onClick={redirectToDetailsPage}
+        className="see-details-button"
+        icon={faArrowRight}
+      />
+      <div className="beer-picture-container">
+        <img
+          src={beer.image_url}
+          className="beer-picture"
+          alt={`Picture of ${beer.name} beer.`}
+          loading="lazy"
+        />
+      </div>
+      <div className="beer-info">
+        <div className="beer-name">{beer.name}</div>
+        <div className="beer-tagline">{beer.tagline}</div>
+      </div>
+      <div className="beer-stats">
+        <Chip label="ABV" value={beer.abv} />
+        <Chip label="IBU" value={beer.ibu} />
+      </div>
+    </div>
+  );
 };
 
 export default BeerTile;
--- src/config/ApiConfig.ts
+++ src/config/ApiConfig.ts
@@ -1,1 +1,1 @@
-export const API_URL = process.env.REACT_APP_API_URL ?? "https://api.punkapi.com/v2/";
+export const API_URL = process.env.REACT_APP_API_URL ?? 'https://api.punkapi.com/v2/';
