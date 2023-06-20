6184f500e94859baed15ae6a9865d5e73b391121
Hubert Cywka <hcywka@gmail.com>
2023-06-20 21:45:33 +0200

    EX-4: Created Navbar component

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -3,11 +3,17 @@ import { ROUTE } from '../constants/RouteConstants';
 import HomePage from '../pages/home/HomePage';
 import { BeerDetailsPage } from '../pages/beer-details/BeerDetailsPage';
 import NotFoundPage from '../pages/error/NotFoundPage';
+import Navbar from '../components/navbar/Navbar';
 
 const AppRouter = createBrowserRouter([
   {
     path: ROUTE.HOME,
-    element: <Outlet />,
+    element: (
+      <>
+        <Navbar />
+        <Outlet />
+      </>
+    ),
     children: [
       { path: ROUTE.HOME, element: <HomePage /> },
       { path: ROUTE.DETAILS, element: <BeerDetailsPage /> }
